b3e5a45297b9bd7d78c794a6cda3faa8ba9e18d8
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-07-18 11:33:53 +0530

    1 change

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,16 @@
 const { override, addBabelPreset, addBabelPlugin } = require('customize-cra');
 
+const addBabelPluginWithLooseMode = (pluginName) => {
+  return (config) => {
+    return addBabelPlugin([pluginName, { loose: true }])(config);
+  };
+};
+
 module.exports = override(
-  addBabelPreset('@babel/preset-env'),
+  addBabelPreset(['@babel/preset-env', { loose: true }]),
   addBabelPreset('@babel/preset-react'),
   addBabelPreset('@babel/preset-typescript'),
-  addBabelPlugin('@babel/plugin-proposal-private-property-in-object')
+  addBabelPluginWithLooseMode('@babel/plugin-proposal-private-property-in-object'),
+  addBabelPluginWithLooseMode('@babel/plugin-transform-class-properties'),
+  addBabelPluginWithLooseMode('@babel/plugin-transform-private-methods')
 );
