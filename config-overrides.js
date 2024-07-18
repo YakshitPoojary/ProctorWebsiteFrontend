dcdaca9636c4ee8ade62ff3504d955d6c0dfdedd
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-07-18 11:29:40 +0530

    made 2 new files

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const { override } = require('customize-cra');
+const { addBabelPreset, addBabelPlugin } = require('customize-cra');
+
+module.exports = override(
+  addBabelPreset('@babel/preset-env'),
+  addBabelPreset('@babel/preset-react'),
+  addBabelPreset('@babel/preset-typescript'),
+  addBabelPlugin('@babel/plugin-proposal-private-property-in-object')
+);
