557e195bcdf3521574d8bc515848f59370d46b97
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-07-25 11:30:07 +0530

    fixed otp verify

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -32,7 +32,6 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [otp, setOtp] = useState('');
   const [otpSent, setOtpSent] = useState(false);
-  const [setOtpVerified] = useState(false);
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [openPasswordModal, setOpenPasswordModal] = useState(false);
@@ -84,7 +83,6 @@ const Login = () => {
     try {
       await axios.post(`${process.env.REACT_APP_BACKEND_API_URL}verify/otp/`, { email, otp });
       setMessage('OTP verified. You can now reset your password.');
-      setOtpVerified(true);
       setOpenPasswordModal(true);
       // Commented out setOpen(false) to keep OTP modal open until reset password completes
       // setOpen(false);
